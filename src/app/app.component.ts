2753f976351fc51f4e4b1bef17fba2317674bf0f
Luis Ricardo <lrtf.developer@gmail.com>
2019-12-19 22:52:51 -0300

    subindo projeto angular alurapic

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { PhotoService } from './photos/photo/photo.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
+
